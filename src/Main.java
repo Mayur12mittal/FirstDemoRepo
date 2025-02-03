87fddd25df525ea3d33e30abf6045eca2b600335
Mayur Mittal <mayur12.cvps@gmail.com>
2025-02-03 18:51:22 +0530

    Update 1: Text Addition

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Mayur");
-
+        System.out.println("This project is used for Git Demo");
     }
 }
